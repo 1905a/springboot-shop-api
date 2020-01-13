3230bf17d04f5ac4e5e965d9695d62b5b34ee0cd
name <812481478@qq.com>
2020-01-13 10:42:16 +0800

    Initial commit

---
+++ src/main/java/com/fh/OrderPortalApplication.java
@@ -0,0 +1,14 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderPortalApplication.class, args);
+    }
+
+
+}
